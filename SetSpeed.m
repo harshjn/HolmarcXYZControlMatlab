91ea23fabb97abf4111ce76c9dc4f1a3e35d8919
harshjn <harshjainldh@gmail.com>
2018-08-10 13:30:05 -0700

    Update SetSpeed.m

--- SetSpeed.m
+++ SetSpeed.m
@@ -1,4 +1,5 @@
 function out1=SetSpeed(s)
+% Set a speed of 4000 steps per second
 
 fwrite(s,hex2dec('64'));o=dec2hex(double(fscanf(s,'%c',1)))
 fwrite(s,hex2dec('22'));o=dec2hex(double(fscanf(s,'%c',1)))
