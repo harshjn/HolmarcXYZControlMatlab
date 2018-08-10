e17a7c9c5e8817143ca42bea3948bbc4294c2e52
harshjn <harshjainldh@gmail.com>
2018-08-10 13:28:39 -0700

    Create SetSpeed.m

---
+++ SetSpeed.m
@@ -0,0 +1,12 @@
+function out1=SetSpeed(s)
+
+fwrite(s,hex2dec('64'));o=dec2hex(double(fscanf(s,'%c',1)))
+fwrite(s,hex2dec('22'));o=dec2hex(double(fscanf(s,'%c',1)))
+fwrite(s,hex2dec('ff'));o=dec2hex(double(fscanf(s,'%c',1)))
+fwrite(s,hex2dec('19'));o=dec2hex(double(fscanf(s,'%c',1)))
+
+fwrite(s,hex2dec('00'));o=dec2hex(double(fscanf(s,'%c',1)))
+
+out1='done';
+
+end
